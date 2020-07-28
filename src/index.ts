1bc9b5b7c14d6dff1b837f544c01e8f4862bb284
Larrygf02 <raulgf_02@hotmail.com>
2020-07-28 12:23:57 -0500

    subscribe and unsubscribe

--- src/index.ts
+++ src/index.ts
@@ -6,16 +6,28 @@ const observer: Observer<any> = {
     complete: () => console.info('[Completado]')
 }
 
-const intervalo$ = new Observable(subs => {
+const intervalo$ = new Observable<number>(subs => {
     // crear un contador
     let number = 0
-    setInterval(() => {
+    const interval = setInterval(() => {
         subs.next(number)
         number += 1;
-        if (number == 10) {
-            subs.complete()
-        }
+        console.log(number)
     }, 1000)
+
+    return () => {
+        clearInterval(interval)
+        console.log('Intervalo destruido')
+    }
 })
 
-intervalo$.subscribe(console.log)
+const subs1 = intervalo$.subscribe()
+const subs2 = intervalo$.subscribe()
+const subs3 = intervalo$.subscribe()
+
+setTimeout(() => {
+    subs1.unsubscribe()
+    subs2.unsubscribe()
+    subs3.unsubscribe()
+    console.log('Completado timeout')
+}, 3000)
