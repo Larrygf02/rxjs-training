c8cf3b1e2932f40b56fd3dad78850f27b2b85987
Larrygf02 <raulgf_02@hotmail.com>
2020-07-28 12:02:07 -0500

    Create observer

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
-import { Observable } from 'rxjs';
+import { Observable, Observer } from 'rxjs';
+
+const observer: Observer<any> = {
+    next: value => console.log('[next]: ', value),
+    error: error => console.warn('[error]:', error),
+    complete: () => console.info('[Completado]')
+}
 
 //const obs$ = Observable.create()
 // observable de tipo string
 const obs$ = new Observable<string>(subs => {
     subs.next('Hola');
     subs.next('Mundo');
+
+    //Forzar un error
+    //const a = undefined
+    //a.nombre = 'Raul'
     // cuando se emite el complete, el 
     // observable ya no emite notificacion
     subs.complete();
@@ -12,4 +22,10 @@ const obs$ = new Observable<string>(subs => {
     subs.next('Este mensaje ya no se notificará')
 });
 
-obs$.subscribe(console.log)
+obs$.subscribe(observer)
+
+/* obs$.subscribe(
+    valor => console.log('next: ', valor),
+    error => console.warn('error: ', error),
+    () => console.info('Completado')
+); */
