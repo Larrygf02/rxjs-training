29c85e1b71bdbac92e26af3381799b6f2a7a6e52
Larrygf02 <raulgf_02@hotmail.com>
2020-07-28 11:49:27 -0500

    Primer observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
+import { Observable } from 'rxjs';
 
+//const obs$ = Observable.create()
+// observable de tipo string
+const obs$ = new Observable<string>(subs => {
+    subs.next('Hola');
+    subs.next('Mundo');
+    // cuando se emite el complete, el 
+    // observable ya no emite notificacion
+    subs.complete();
 
-console.log('Hola Mundo!');
+    subs.next('Este mensaje ya no se notificará')
+});
 
-
-
-
-
-
-
+obs$.subscribe(console.log)
