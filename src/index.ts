40cdbee372570746db4b225ace2a2460c29ed0a3
Larrygf02 <raulgf_02@hotmail.com>
2020-07-29 22:34:07 -0500

    map to rxjs

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 
-import { range, fromEvent } from 'rxjs';
-import { map, pluck, mapTo } from 'rxjs/operators'
+import { fromEvent } from 'rxjs';
+import { mapTo } from 'rxjs/operators'
 
 const keyup$ = fromEvent<KeyboardEvent>(document, 'keyup');
 
