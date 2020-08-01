ec26a8314ed13fede4846572030cf817a67c82a9
Larrygf02 <raulgf_02@hotmail.com>
2020-08-01 13:30:31 -0500

    ajax rxjs

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,29 @@ import { of } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 
 // api para hacer pruebas
-const url = 'https://httpbxin.org/delay/1';
+const url = 'https://httpbin.org/delay/1';
+
+ajax.post(url,{
+    id: 1,
+    nombre: 'Grace'
+}, {
+    'mi-token': 'ABC123'
+}).subscribe(console.log)
+
+
+ajax({
+    url: url,
+    method: 'POST',
+    headers: {
+        'mi-token': 'ABC123'
+    },
+    body: {
+        id: 1,
+        nombre: 'Peque'
+    }
+}).subscribe(console.log)
 
 const manejaError = (resp: AjaxError) => {
     console.warn('error', resp.message)
     return of({})
 }
-
-/* const obs$ = ajax.getJSON(url).pipe(
-    catchError(manejaError)
-)
-const obs2$ = ajax(url).pipe(
-    catchError(manejaError)
-); */
-
-const obs$ = ajax.getJSON(url)
-const obs2$ = ajax(url)
-obs$.subscribe({
-    next: val => console.log('next: ', val),
-    error: err => console.warn('error en subs: ', err),
-    complete: () => console.log('complete')
-});
-//obs2$.subscribe(data => console.log('ajax', data))
