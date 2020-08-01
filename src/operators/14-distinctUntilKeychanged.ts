2cd1cfd383f523f2e8fe2453db80733a5dc6fb57
Larrygf02 <raulgf_02@hotmail.com>
2020-07-31 20:17:23 -0500

    distinct until key changed

---
+++ src/operators/14-distinctUntilKeychanged.ts
@@ -0,0 +1,43 @@
+import { of, from } from 'rxjs';
+import { tap, distinctUntilChanged, distinctUntilKeyChanged } from 'rxjs/operators';
+const numeros = [1,1,2,3,4,4,1]
+
+const numeros$ = of(...numeros)
+numeros$.pipe(
+    //tap(val => console.log('antes del distinct', val)),
+    distinctUntilChanged()
+).subscribe(console.log)
+
+interface Personaje {
+    nombre: string
+}
+
+const personajes: Personaje[] = [
+    {
+        nombre: 'Megaman'
+    },
+    {
+        nombre: 'X'
+    },
+    {
+        nombre: 'Acuaman'
+    },
+    {
+        nombre: 'Hola'
+    },
+    {
+        nombre: 'Hola'
+    },
+    {
+        nombre: 'Megaman'
+    },
+    {
+        nombre: 'Megaman'
+    }
+]
+
+// distinct in objects
+from(personajes).pipe(
+    //distinctUntilChanged((ant, act) => ant.nombre === act.nombre)
+    distinctUntilKeyChanged('nombre')
+).subscribe(console.log)
