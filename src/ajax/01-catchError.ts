b8273a3a8a5f51ae78905cfee90c09b9bd807d4d
Larrygf02 <raulgf_02@hotmail.com>
2020-08-01 13:05:54 -0500

    catch Error rxjs ajax

---
+++ src/ajax/01-catchError.ts
@@ -0,0 +1,28 @@
+import { ajax } from 'rxjs/ajax'
+import { map, pluck, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
+const url = 'https://api.github.com/userxxs?per_page=5';
+
+const manejaErrores = (response: Response) => {
+    if (!response.ok) {
+        throw new Error(response.statusText)
+    }
+    return response;
+}
+
+const fetchPromesa = fetch(url);
+
+ajax(url).pipe(
+    //map( resp => resp.response)
+    pluck('response'),
+    catchError(err => {
+        return of([]);
+    })
+).subscribe(console.log)
+/* fetchPromesa
+    .then(manejaErrores)
+    .then(resp => resp.json())
+    .then(data => console.log('data', data))
+    .catch(err => console.warn('error en usuarios', err)) */
+
+
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-const url = 'https://api.github.com/usexs?per_page=5';
-
-const fetchPromesa = fetch(url);
-
-fetchPromesa
-    .then(resp => resp.json())
-    .then(console.log)
-    .catch(err => console.warn('error en usuarios', err))
-
