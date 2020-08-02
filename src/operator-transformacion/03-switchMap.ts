1382a40a720a79d0a5edcbd907accdf3faa98d20
Larrygf02 <raulgf_02@hotmail.com>
2020-08-01 20:39:27 -0500

    merge vs switch map rxjs

---
+++ src/operator-transformacion/03-switchMap.ts
@@ -0,0 +1,42 @@
+import { fromEvent } from 'rxjs';
+import { debounceTime, map, debounce, pluck, mergeAll, mergeMap, switchMap } from 'rxjs/operators';
+import { ajax } from 'rxjs/ajax';
+
+const body = document.querySelector('body')
+const textInput = document.createElement('input')
+const orderList = document.createElement('ol')
+
+body.append(textInput, orderList)
+
+// Streams
+const input$ = fromEvent<KeyboardEvent>(textInput, 'keyup')
+// antes
+/* input$.pipe(
+    debounceTime(500),
+    map(  event => {
+        const texto = event.target['value'];
+        return ajax.getJSON(
+            `https://api.github.com/search/users?q=${texto}`
+        )
+    })
+).subscribe(resp => {
+    resp.subscribe(console.log)
+}) */
+
+// despues
+input$.pipe(
+    debounceTime(500),
+    pluck('target', 'value'),
+    mergeMap(texto => ajax.getJSON(
+        `https://api.github.com/search/users?q=${texto}`
+    )),
+    pluck('items')
+)/* .subscribe(resp => {
+    console.log(resp[0].score)
+}) */
+
+const url = 'https://httpbin.org/delay/1?arg=';
+input$.pipe(
+    pluck('target', 'value'),
+    switchMap(texto => ajax.getJSON(url+texto))
+).subscribe(console.log)
