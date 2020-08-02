6ba4d1e0b1c8ef45b667476c87cf280bfbc31d25
Larrygf02 <raulgf_02@hotmail.com>
2020-08-01 19:04:20 -0500

    mergeAll rxjs

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { fromEvent } from 'rxjs';
-import { debounceTime, map } from 'rxjs/operators';
+import { debounceTime, map, debounce, pluck, mergeAll } from 'rxjs/operators';
 import { ajax } from 'rxjs/ajax';
 
 const body = document.querySelector('body')
@@ -10,15 +10,29 @@ body.append(textInput, orderList)
 
 // Streams
 const input$ = fromEvent<KeyboardEvent>(textInput, 'keyup')
-input$.pipe(
+// antes
+/* input$.pipe(
     debounceTime(500),
     map(  event => {
         const texto = event.target['value'];
         return ajax.getJSON(
-            `https://api.github.com/users/${texto}`
+            `https://api.github.com/search/users?q=${texto}`
         )
     })
 ).subscribe(resp => {
     resp.subscribe(console.log)
+}) */
+
+// despues
+input$.pipe(
+    debounceTime(500),
+    pluck('target', 'value'),
+    map(texto => ajax.getJSON(
+        `https://api.github.com/search/users?q=${texto}`
+    )),
+    mergeAll(),
+    pluck('items')
+).subscribe(resp => {
+    console.log(resp)
 })
 
