0345a432c84255ffb88f535aaa640d4ef54a7953
Larrygf02 <raulgf_02@hotmail.com>
2020-08-02 20:45:53 -0500

    forkjoin lab rxjs

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,12 @@
-import { of, interval, forkJoin } from 'rxjs';
-import { take, delay } from 'rxjs/operators';
+import { forkJoin } from "rxjs";
+import { ajax } from "rxjs/ajax";
 
-const numeros$ = of(1,2,3,4);
-const intervalo$ = interval(1000).pipe(take(3))
-const letras$ = of('a', 'b', 'c').pipe(delay(3500))
 
-forkJoin(
-    numeros$,
-    intervalo$,
-    letras$
-).subscribe(console.log)
+const GITHUB_API_URL = 'https://api.github.com/users';
+const GITHUB_USER = 'klerith';
 
 forkJoin({
-    numeros$,
-    intervalo$,
-    letras$
+    usuario: ajax.getJSON(`${GITHUB_API_URL}/${GITHUB_USER}`),
+    repos: ajax.getJSON(`${GITHUB_API_URL}/${GITHUB_USER}/repos`),
+    gists: ajax.getJSON(`${GITHUB_API_URL}/${GITHUB_USER}/gists`)
 }).subscribe(console.log)
