8d48452f191d678a8f7940e1c44a06d7bafa0c34
Larrygf02 <raulgf_02@hotmail.com>
2020-08-02 21:17:49 -0500

    reduce exercise rxjs

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
-import { forkJoin } from "rxjs";
-import { ajax } from "rxjs/ajax";
+import { from } from 'rxjs';
+import { filter, reduce } from 'rxjs/operators';
+
+/**
+ * Ejercicio: 
+ * Sume todos los números del arreglo usando un reduce.
+ * Debe de filtrar para que sólo números sean procesados
+ * La salida debe de ser 32
+ * 
+ * Tip:
+ * isNan() es una función de JavaScript para determinar si es número
+ * Usar filter<any>(...) para no tener problemas de tipado.
+ */
+
+(() =>{
 
 
-const GITHUB_API_URL = 'https://api.github.com/users';
-const GITHUB_USER = 'klerith';
+  const datos = [1, 2, 'foo', 3, 5, 6, 'bar', 7, 8];
 
-forkJoin({
-    usuario: ajax.getJSON(`${GITHUB_API_URL}/${GITHUB_USER}`),
-    repos: ajax.getJSON(`${GITHUB_API_URL}/${GITHUB_USER}/repos`),
-    gists: ajax.getJSON(`${GITHUB_API_URL}/${GITHUB_USER}/gists`)
-}).subscribe(console.log)
+  from(datos).pipe(
+    // Trabajar aquí
+    filter<any>(val => !isNaN(val)),
+    reduce((a,b) => a + b)
+  ).subscribe( console.log ) // La salida debe de ser 32
+
+})();
+
+		
